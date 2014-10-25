98d2827fc57c3e3afe0f4885c003297933c57364
Dmitry Dorofeev <dorof21@yandex.ru>
2014-10-25 15:30:07 +0400

    prerender profile to logut

--- public_html/js/views/profile.js
+++ public_html/js/views/profile.js
@@ -5,7 +5,8 @@ define([
 ], function (Backbone, tmpl, userModel) {
   var ProfileView = Backbone.View.extend({
     initialize: function() {
-      this.listenTo(this.model, 'change', this.render);
+        this.render();
+        this.listenTo(this.model, 'change', this.render);
     },
     events: {
         'click .js-logout': 'logout'
