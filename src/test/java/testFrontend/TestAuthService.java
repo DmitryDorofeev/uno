a329beff08af43e3f6140a40821561126304a5ee
Alexey <aleksei_halaidzh@mail.ru>
2014-11-15 21:26:28 +0300

    Тестирование тестов :))

--- src/test/java/testFrontend/TestAuthService.java
+++ src/test/java/testFrontend/TestAuthService.java
@@ -2,6 +2,7 @@ package testFrontend;
 
 import static org.junit.Assert.*;
 
+import base.AuthService;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,7 +13,7 @@ import frontend.AuthServiceImpl;
  * Created by К on 30.10.2014.
  */
 public class TestAuthService {
-    public AuthServiceImpl testAuthService = new AuthServiceImpl();
+    public AuthService testAuthService = new AuthServiceImpl();
     public UserProfileImpl testUsers[] = new UserProfileImpl[8];
 
     @Before
@@ -29,29 +30,53 @@ public class TestAuthService {
 
     @Test
     public void testSignUp() {
-        initTestValues();
-        assertEquals("No registered nologin", false, testAuthService.signUp(testUsers[0]));
-        assertEquals("No registered nopswd", false, testAuthService.signUp(testUsers[1]));
-        assertEquals("Registered noemail", true, testAuthService.signUp(testUsers[2]));
-        assertEquals("Already registered test", false, testAuthService.signUp(testUsers[3]));
-        assertEquals("Registered gooduser", true, testAuthService.signUp(testUsers[7]));
+        System.out.append("1\n");
+//        assertEquals("No registered nologin", false, testAuthService.signUp(testUsers[0]));
+//        assertEquals("No registered nopswd", false, testAuthService.signUp(testUsers[1]));
+//        assertEquals("Registered noemail", true, testAuthService.signUp(testUsers[2]));
+//        assertEquals("Already registered test", false, testAuthService.signUp(testUsers[3]));
+//        assertEquals("Registered gooduser", true, testAuthService.signUp(testUsers[7]));
+        for (int i = 0; i < 1000000; ++i);
+        System.out.append("13\n");
     }
 
     @Test
     public void testSignInAndSighOut() {
-        initTestValues();
-        assertEquals("Logined" + testUsers[2].getLogin(), true, testAuthService.signIn("1", testUsers[2].getLogin(), testUsers[2].getPass()));
-        assertEquals("No such user", false, testAuthService.signIn("2", testUsers[4].getLogin(), testUsers[4].getPass()));
-        assertEquals("No logined: password is empty", false, testAuthService.signIn("3", testUsers[5].getLogin(), testUsers[5].getPass()));
-        assertEquals("No logined: login is empty", false, testAuthService.signIn("4", testUsers[6].getLogin(), testUsers[6].getPass()));
-        assertEquals("Logined" + testUsers[3].getLogin(), true, testAuthService.signIn("5", testUsers[3].getLogin(), testUsers[3].getPass()));
-
-        assertEquals("Logout" + testUsers[2].getLogin(), true, testAuthService.logOut("1"));
-        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("2"));
-        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("3"));
-        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("4"));
-        assertEquals("Logout" + testUsers[3].getLogin(), false, testAuthService.logOut("5"));
-        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("6"));
+        System.out.print("2\n");
+//        assertEquals("Logined" + testUsers[2].getLogin(), true, testAuthService.signIn("1", testUsers[2].getLogin(), testUsers[2].getPass()));
+//        assertEquals("No such user", false, testAuthService.signIn("2", testUsers[4].getLogin(), testUsers[4].getPass()));
+//        assertEquals("No logined: password is empty", false, testAuthService.signIn("3", testUsers[5].getLogin(), testUsers[5].getPass()));
+//        assertEquals("No logined: login is empty", false, testAuthService.signIn("4", testUsers[6].getLogin(), testUsers[6].getPass()));
+//        assertEquals("Logined" + testUsers[3].getLogin(), true, testAuthService.signIn("5", testUsers[3].getLogin(), testUsers[3].getPass()));
+//
+//        assertEquals("Logout" + testUsers[2].getLogin(), true, testAuthService.logOut("1"));
+//        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("2"));
+//        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("3"));
+//        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("4"));
+//        assertEquals("Logout" + testUsers[3].getLogin(), false, testAuthService.logOut("5"));
+//        assertEquals("No logined user with this SessionId", false, testAuthService.logOut("6"));
+        for (int i = 0; i < 1000000; ++i);
+        System.out.print("23\n");
     }
 
+    @Test
+    public void test3() {
+        System.out.print("6\n");
+        for (int i = 0; i < 1000000; ++i);
+        System.out.print("63\n");
+    }
+
+    @Test
+    public void test4() {
+        System.out.print("4\n");
+        for (int i = 0; i < 1000000; ++i) ;
+        System.out.append("43\n");
+    }
+
+    @Test
+    public void test5() {
+        System.out.print("5\n");
+        for (int i = 0; i < 1000000; ++i);
+        System.out.print("53\n");
+    }
 }
