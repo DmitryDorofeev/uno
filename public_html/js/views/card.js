ac4c07d587cb5ca0bf6bba875935ba0faa5f872e
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2014-11-20 23:06:05 +0300

    карты выводятся верно

--- public_html/js/views/card.js
+++ public_html/js/views/card.js
@@ -15,11 +15,12 @@ define([
         },
         render: function () {
             this.$el.css({
-                'background-position': '' + this.model.get('x') + 'px ' + this.model.get('y') + 'px',
+                'background-position': '-' + (this.model.get('x')/2) + 'px -' + (this.model.get('y')/2) + 'px',
                 'background-size': '1681px, 1141px'
             })
             .width(this.model.get('width')/2)
-            .height(this.model.get('height')/2);
+            .height(this.model.get('height')/2)
+            .data('id', this.model.get('cardId'));
             return this;
         }
     });
