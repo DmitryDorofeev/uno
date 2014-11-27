18779dba25a93ed0e1060bef6f50033d01e995fa
KPK10 <k_kobzisty@mail.ru>
2014-11-27 17:45:36 +0300

    недописанный тест

--- src/test/java/testFrontend/TestSignUpServlet.java
+++ src/test/java/testFrontend/TestSignUpServlet.java
@@ -1,7 +1,50 @@
 package testFrontend;
 
+import base.SignUpServlet;
+import base.UserProfile;
+import db.UserProfileImpl;
+import frontend.AuthServiceImpl;
+import frontend.SignUpServletImpl;
+import org.eclipse.jetty.server.Response;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import static org.mockito.Mockito.*;
 /**
  * Created by К on 20.11.2014.
  */
 public class TestSignUpServlet {
+    public static UserProfile testUsers[] = new UserProfileImpl[5];
+    SignUpServlet testSignUpServlet = new SignUpServletImpl(new AuthServiceImpl());
+    final private static HttpServletRequest testRequest = mock(HttpServletRequest.class);
+    private Response testResponse = new Response.;
+
+    @BeforeClass
+    public void initTestValues() {
+        testUsers[0] = new UserProfileImpl("","123qaz!", "nologin");
+        testUsers[1] = new UserProfileImpl("nopswd","", "nopassword");
+        testUsers[2] = new UserProfileImpl("noemail","12345qaz!", "");
+        testUsers[3] = new UserProfileImpl("test","test", "test");
+        testUsers[4] = new UserProfileImpl("testnouser","testnouser", "testnouser");
+    }
+
+    @Test
+    public void testDoPost() throws Exception {
+        for (int i = 0; i < 5; ++i) {
+            when(testRequest.getParameter("login")).thenReturn(testUsers[i].getLogin());
+            when(testRequest.getParameter("email")).thenReturn(testUsers[i].getEmail());
+            when(testRequest.getParameter("password")).thenReturn(testUsers[i].getPass());
+
+            testSignUpServlet.doPost(testRequest, testResponse);
+        }
+
+        verify(testRequest, times(5)).getParameter("login");
+        verify(testRequest, times(5)).getParameter("email");
+        verify(testRequest, times(5)).getParameter("password");
+    }
+
 }
