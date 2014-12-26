9945ad90ffdcd6ca080b54697968ec62caeeaf8a
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2014-12-27 02:25:20 +0300

    тст

--- public_html/js/tests/cardcollectiontests/cards.js
+++ public_html/js/tests/cardcollectiontests/cards.js
@@ -3,13 +3,14 @@ define([
     'jquery',
     'collections/cards',
     'models/card',
+    'models/game',
     'mock-socket'
-], function (Backbone, $, CardCollection, CardModel) {
+], function (Backbone, $, CardCollection, CardModel, gameModel) {
     window.WebSocket = MockSocket;
 
     var exampleServer = new WebSocketServer('ws://' + location.host + '/game');
-    exampleServer.on('connection', function(server) {
-        server.on('message', function(msg) {
+    exampleServer.on('connection', function (server) {
+        server.on('message', function (msg) {
 
             var data = JSON.parse(msg.data);
             if (data.type === 'card') {
@@ -28,19 +29,18 @@ define([
                     type: 'start',
                     body: {}
                 }));
-                            }
+            }
 
         });
     });
 
     var run = function () {
-        module ('Card collection test');
-        CardCollection.connect();
+        module('Card collection test');
+        gameModel.connect();
         asyncTest('cardCollection.sendCards', function () {
             var cardModel = new CardModel();
             cardModel.set('cardId', 1);
             CardCollection.sendCard(cardModel).done(function () {
-                // debugger;
                 start();
                 ok(true, 'Testing of sending card is correct');
             }).fail(function () {
@@ -48,4 +48,7 @@ define([
                 ok(false, 'Testing of sending card is not correct');
             });
         });
-    }
+    };
+
+    return {run: run};
+});
