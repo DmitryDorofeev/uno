ed8d33777202cbb20f01da31e276eaa4fc1e5e04
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2015-01-25 18:43:38 +0300

    fix scripts && jar file

--- assemble.bat
+++ assemble.bat
@@ -2,6 +2,6 @@
 
 mvn compile assembly:single
 echo assemble done
-copy .\target\L1.2-1.0-jar-with-dependencies.jar .\
+copy .\target\uno-1.0-jar-with-dependencies.jar .\
 echo copy done
 pause
--- assemble.sh
+++ assemble.sh
@@ -1,3 +1,3 @@
 mvn compile assembly:single
-rm ./L1.2-1.0-jar-with-dependencies.jar
-cp ./target/L1.2-1.0-jar-with-dependencies.jar ./
+rm ./uno-1.0-jar-with-dependencies.jar
+cp ./target/uno-1.0-jar-with-dependencies.jar ./
