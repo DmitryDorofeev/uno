43c97f3f07d5444ca78f6dcd1c2587a98ef20118
DDServer <you@dmitrydorofeev.ru>
2015-01-29 07:16:13 -0500

    uno runner

--- runner/index.js
+++ runner/index.js
@@ -6,24 +6,26 @@ var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var crypto = require('crypto');
-var shasum = crypto.createHash('sha1');
 
 var readFile = function (input) {
     var output = '';
     var json;
     var index = input.indexOf('\n');
     while (index > -1) {
-        var line = input.substring(0, index - 1);
+        var line = input.substring(0, index);
         input = input.substring(index + 1);
         try {
+	        console.log(line);
             json = JSON.parse(line);
-            output += line;
+            output += "<div class=\"line\">" + (json.type || json.title) + "</div>\n";
+            output += "<div class=\"greenline\">" + JSON.stringify((json.message || json.body)) + "</div>\n";
         }
         catch (e) {
-            output += "<div class=\"redline\">" + line + "</div>";
+            output += "<div class=\"redline\">" + line + "</div>\n";
         }
         index = input.indexOf('\n');
     }
+    return output;
 }
 
 app.use(bodyParser.urlencoded({
@@ -34,6 +36,7 @@ app.post('/restart', function (req, res) {
 	console.log('request');
 	res.writeHead(200, { 'Content-Type': 'text/html' });
     var pass = req.body.pass;
+    var shasum = crypto.createHash('sha1');
     shasum.update(pass);
     if (shasum.digest('hex') == '90c7f35fdaf7557938bf8f0d4bf85f3ea3719286') {
 	    console.log('pass ok');
@@ -51,8 +54,22 @@ app.post('/restart', function (req, res) {
     res.end('ok');
 });
 
-app.post('/remove', function (req, res) {
-	res.send('ok');
+app.post('/deploy', function (req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+    var pass = req.body.pass;
+    console.log('PASS', pass);
+    var shasum = crypto.createHash('sha1');
+    shasum.update(pass);
+    if (shasum.digest('hex') == '90c7f35fdaf7557938bf8f0d4bf85f3ea3719286') {
+	    console.log('pass ok');
+        shell.cd('/var/www/uno');
+        var info = shell.exec('git pull origin master');
+        console.log('GIT OUT ', info);
+        res.end(info.output);
+    }
+    else {
+	    res.end('ok');
+    }
 });
 
 app.use('/', express.static(__dirname + '/public'));
@@ -63,6 +80,12 @@ io.on('connection', function(socket) {
 	    io.emit('file', readFile(fs.readFileSync('/var/www/uno/nohup.out', "utf8")));
 	    fs.watchFile('/var/www/uno/nohup.out', function(curr,prev) {
 	        io.emit('file', readFile(fs.readFileSync('/var/www/uno/nohup.out', "utf8")));
+	    });
+    });
+    
+    socket.on('log', function (msg) {
+	    fs.appendFile('/var/www/uno/nohup.out', msg, function (err) {
+		    
 	    });
     });
 });
